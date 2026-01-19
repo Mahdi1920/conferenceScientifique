d2fb0329b5fd725d32c27d41f4cb1b3959841fe0
mahdi <mahdichabbouh98@gmail.com>
2026-01-19 19:30:33 +0100

    Add validators for Conference and User models to enforce data integrity

--- conferenceScientifique/ConferenceApp/models.py
+++ conferenceScientifique/ConferenceApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
 from UserApp.models import User
+from django.core.validators import MinLengthValidator, MaxLengthValidator, FileExtensionValidator
 
 # Create your models here.
 class Conference(models.Model):
@@ -11,12 +11,12 @@ class Conference(models.Model):
         ('WD', 'Web Development'),
         ('CY', 'Cybersecurity'),    
     ]
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,validators=[MinLengthValidator(5, "Conference name must be at least 5 characters long.")])
     theme = models.CharField(max_length=2, choices=THEME_CHOICES)
     location = models.CharField(max_length=200)
     start_date = models.DateField()
-    end_date = models.DateField()
-    description = models.TextField()
+    end_date = models.DateField() 
+    description = models.TextField(validators=[MaxLengthValidator(300, "Description cannot exceed 300 characters."), MinLengthValidator(20, "Description must be at least 20 characters long.")])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -38,7 +38,7 @@ class Submission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     abstract = models.TextField()
-    paper = models.FileField(upload_to='submissions/')
+    paper = models.FileField(upload_to='submissions/',validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
     submission_date = models.DateTimeField(auto_now_add=True)
     keywords = models.CharField(max_length=200)
     paid = models.BooleanField(default=False)
--- conferenceScientifique/UserApp/models.py
+++ conferenceScientifique/UserApp/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.forms import ValidationError
 
 # Create your models here.
+name_validator=RegexValidator(
+    regex='^[a-zA-Z]+$', 
+    message='Name must contain only alphabetic characters.'
+    )
+
+def valdidateEmail(value):
+    allowed_domains = ['esprit.tn', 'univ.tn', 'mit.edu']
+    domain = value.split('@')[-1]
+    if domain not in allowed_domains:
+        raise ValidationError(f'Email domain must be one of the following: {", ".join(allowed_domains)}')
 class User(AbstractUser):
     Role_CHOICES = [
         ('particpant', 'Participant'),
@@ -9,9 +21,9 @@ class User(AbstractUser):
         ('member', 'Member'),
     ]
     user_id = models.CharField(primary_key=True, max_length=100, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, validators=[name_validator])
+    last_name = models.CharField(max_length=30, validators=[name_validator])
+    email = models.EmailField(unique=True, validators=[valdidateEmail])
     role = models.CharField(max_length=50, choices=Role_CHOICES)
     nationality = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
